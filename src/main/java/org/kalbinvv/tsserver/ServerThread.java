a810b85da9c3305c33beeec06ee4a3fffb926edc
Kalbin Vladimir <kalbin.wow@yandex.ru>
2022-03-25 17:53:28 +0500

    Added configs support and fixed class path.

--- src/main/java/org/kalbinvv/tsserver/ServerThread.java
+++ src/main/java/org/kalbinvv/tsserver/ServerThread.java
@@ -5,16 +5,19 @@ import java.io.IOException;
 
 import java.net.ServerSocket;
 
+import org.kalbinvv.tscore.config.Config;
 import org.kalbinvv.tscore.net.Connection;
 import org.kalbinvv.tscore.net.Response;
 
 public class ServerThread implements Runnable{
 	
 	private ServerSocket serverSocket;
-	private static int serverPort = 2090;
 	
 	@Override
 	public void run() {
+		Config config = new Config("config.yml");
+		Integer serverPort = config.getInteger("serverPort");
+		System.out.println("Server is running on port: " + serverPort);
 		ServerHandler serverHandler = TestingSystemServer.getServerHandler();
 		try {
 			serverSocket = new ServerSocket(serverPort);
